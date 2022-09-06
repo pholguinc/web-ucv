94e4df8c66310d7d619b03eb93ac3c76267bba07
Zaphkiel <holguinpedro90@gmail.com>
2022-09-05 21:40:42 -0500

    Initial commit

---
+++ sesion-1/index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!--Titulos HTML-->
+    <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda sed eius totam.|</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, sequi? Earum labore minima nihil! Magnam impedit consectetur veritatis necessitatibus voluptatum laboriosam iusto, 
+        unde veniam beatae. Laboriosam blanditiis temporibus, accusantium non fugit reprehenderit veniam excepturi corporis numquam maiores necessitatibus provident magnam a? Temporibus
+        itaque illum ipsam perferendis non deleniti numquam quam eius, similique neque, consequatur cumque voluptas, dolore sint! Similique odit culpa maxime?
+        Sed doloribus, quod sit aspernatur a voluptatibus eos porro delectus quas modi quibusdam in esse alias autem sint ex tempora quia optio molestias recusandae? 
+        Nulla, autem reiciendis! Possimus, cupiditate vitae perspiciatis laboriosam iusto ipsam commodi aperiam voluptates quam molestiae illo sequi dignissimos nam 
+        beatae eos voluptatem quas quidem accusamus temporibus explicabo animi necessitatibus reiciendis eaque! Commodi deleniti exercitationem, in similique recusandae rerum doloremque odio,
+         earum a accusamus numquam beatae delectus est nemo enim? Dolore quos repudiandae eligendi error corrupti veniam in ex vel modi accusamus corporis, est id cumque mollitia iure itaque 
+         voluptatibus accusantium, inventore reprehenderit atque ab neque! Doloribus praesentium iure eligendi quibusdam laboriosam voluptate quidem, accusantium veniam molestias consequatur 
+         sapiente laborum reprehenderit laudantium nostrum neque delectus ea consectetur culpa nihil repellat! Nihil itaque ducimus, 
+        quia sed culpa quisquam laboriosam et ad quod sunt excepturi ratione atque?</p>
+    <hr>
+
+    <h2>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa esse dignissimos voluptatibus!</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, sequi? Earum labore minima nihil! Magnam impedit consectetur veritatis necessitatibus voluptatum laboriosam iusto, 
+        unde veniam beatae. Laboriosam blanditiis temporibus, accusantium non fugit reprehenderit veniam excepturi corporis numquam maiores necessitatibus provident magnam a? Temporibus
+        itaque illum ipsam perferendis non deleniti numquam quam eius, similique neque, consequatur cumque voluptas, dolore sint! Similique odit culpa maxime?
+        Sed doloribus, quod sit aspernatur a voluptatibus eos porro delectus quas modi quibusdam in esse alias autem sint ex tempora quia optio molestias recusandae? 
+        Nulla, autem reiciendis! Possimus, cupiditate vitae perspiciatis laboriosam iusto ipsam commodi aperiam voluptates quam molestiae illo sequi dignissimos nam 
+        beatae eos voluptatem quas quidem accusamus temporibus explicabo animi necessitatibus reiciendis eaque! Commodi deleniti exercitationem, in similique recusandae rerum doloremque odio,
+         earum a accusamus numquam beatae delectus est nemo enim? Dolore quos repudiandae eligendi error corrupti veniam in ex vel modi accusamus corporis, est id cumque mollitia iure itaque 
+         voluptatibus accusantium, inventore reprehenderit atque ab neque! Doloribus praesentium iure eligendi quibusdam laboriosam voluptate quidem, accusantium veniam molestias consequatur 
+         sapiente laborum reprehenderit laudantium nostrum neque delectus ea consectetur culpa nihil repellat! Nihil itaque ducimus, 
+        quia sed culpa quisquam laboriosam et ad quod sunt excepturi ratione atque?</p>
+    <hr>
+
+    <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur dolorum corrupti aspernatur?</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, sequi? Earum labore minima nihil! Magnam impedit consectetur veritatis necessitatibus voluptatum laboriosam iusto, 
+        unde veniam beatae. Laboriosam blanditiis temporibus, accusantium non fugit reprehenderit veniam excepturi corporis numquam maiores necessitatibus provident magnam a? Temporibus
+        itaque illum ipsam perferendis non deleniti numquam quam eius, similique neque, consequatur cumque voluptas, dolore sint! Similique odit culpa maxime?
+        Sed doloribus, quod sit aspernatur a voluptatibus eos porro delectus quas modi quibusdam in esse alias autem sint ex tempora quia optio molestias recusandae? 
+        Nulla, autem reiciendis! Possimus, cupiditate vitae perspiciatis laboriosam iusto ipsam commodi aperiam voluptates quam molestiae illo sequi dignissimos nam 
+        beatae eos voluptatem quas quidem accusamus temporibus explicabo animi necessitatibus reiciendis eaque! Commodi deleniti exercitationem, in similique recusandae rerum doloremque odio,
+         earum a accusamus numquam beatae delectus est nemo enim? Dolore quos repudiandae eligendi error corrupti veniam in ex vel modi accusamus corporis, est id cumque mollitia iure itaque 
+         voluptatibus accusantium, inventore reprehenderit atque ab neque! Doloribus praesentium iure eligendi quibusdam laboriosam voluptate quidem, accusantium veniam molestias consequatur 
+         sapiente laborum reprehenderit laudantium nostrum neque delectus ea consectetur culpa nihil repellat! Nihil itaque ducimus, 
+        quia sed culpa quisquam laboriosam et ad quod sunt excepturi ratione atque?</p>
+    <hr>
+
+    <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore soluta velit iusto?</h4>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, sequi? Earum labore minima nihil! Magnam impedit consectetur veritatis necessitatibus voluptatum laboriosam iusto, 
+        unde veniam beatae. Laboriosam blanditiis temporibus, accusantium non fugit reprehenderit veniam excepturi corporis numquam maiores necessitatibus provident magnam a? Temporibus
+        itaque illum ipsam perferendis non deleniti numquam quam eius, similique neque, consequatur cumque voluptas, dolore sint! Similique odit culpa maxime?
+        Sed doloribus, quod sit aspernatur a voluptatibus eos porro delectus quas modi quibusdam in esse alias autem sint ex tempora quia optio molestias recusandae? 
+        Nulla, autem reiciendis! Possimus, cupiditate vitae perspiciatis laboriosam iusto ipsam commodi aperiam voluptates quam molestiae illo sequi dignissimos nam 
+        beatae eos voluptatem quas quidem accusamus temporibus explicabo animi necessitatibus reiciendis eaque! Commodi deleniti exercitationem, in similique recusandae rerum doloremque odio,
+         earum a accusamus numquam beatae delectus est nemo enim? Dolore quos repudiandae eligendi error corrupti veniam in ex vel modi accusamus corporis, est id cumque mollitia iure itaque 
+         voluptatibus accusantium, inventore reprehenderit atque ab neque! Doloribus praesentium iure eligendi quibusdam laboriosam voluptate quidem, accusantium veniam molestias consequatur 
+         sapiente laborum reprehenderit laudantium nostrum neque delectus ea consectetur culpa nihil repellat! Nihil itaque ducimus, 
+        quia sed culpa quisquam laboriosam et ad quod sunt excepturi ratione atque?</p>
+    <hr>
+
+    <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore soluta velit iusto?</h5>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, sequi? Earum labore minima nihil! Magnam impedit consectetur veritatis necessitatibus voluptatum laboriosam iusto, 
+        unde veniam beatae. Laboriosam blanditiis temporibus, accusantium non fugit reprehenderit veniam excepturi corporis numquam maiores necessitatibus provident magnam a? Temporibus
+        itaque illum ipsam perferendis non deleniti numquam quam eius, similique neque, consequatur cumque voluptas, dolore sint! Similique odit culpa maxime?
+        Sed doloribus, quod sit aspernatur a voluptatibus eos porro delectus quas modi quibusdam in esse alias autem sint ex tempora quia optio molestias recusandae? 
+        Nulla, autem reiciendis! Possimus, cupiditate vitae perspiciatis laboriosam iusto ipsam commodi aperiam voluptates quam molestiae illo sequi dignissimos nam 
+        beatae eos voluptatem quas quidem accusamus temporibus explicabo animi necessitatibus reiciendis eaque! Commodi deleniti exercitationem, in similique recusandae rerum doloremque odio,
+         earum a accusamus numquam beatae delectus est nemo enim? Dolore quos repudiandae eligendi error corrupti veniam in ex vel modi accusamus corporis, est id cumque mollitia iure itaque 
+         voluptatibus accusantium, inventore reprehenderit atque ab neque! Doloribus praesentium iure eligendi quibusdam laboriosam voluptate quidem, accusantium veniam molestias consequatur 
+         sapiente laborum reprehenderit laudantium nostrum neque delectus ea consectetur culpa nihil repellat! Nihil itaque ducimus, 
+        quia sed culpa quisquam laboriosam et ad quod sunt excepturi ratione atque?</p>
+    <hr>
+
+    <h6>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore soluta velit iusto?</h6>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, sequi? Earum labore minima nihil! Magnam impedit consectetur veritatis necessitatibus voluptatum laboriosam iusto, 
+        unde veniam beatae. Laboriosam blanditiis temporibus, accusantium non fugit reprehenderit veniam excepturi corporis numquam maiores necessitatibus provident magnam a? Temporibus
+        itaque illum ipsam perferendis non deleniti numquam quam eius, similique neque, consequatur cumque voluptas, dolore sint! Similique odit culpa maxime?
+        Sed doloribus, quod sit aspernatur a voluptatibus eos porro delectus quas modi quibusdam in esse alias autem sint ex tempora quia optio molestias recusandae? 
+        Nulla, autem reiciendis! Possimus, cupiditate vitae perspiciatis laboriosam iusto ipsam commodi aperiam voluptates quam molestiae illo sequi dignissimos nam 
+        beatae eos voluptatem quas quidem accusamus temporibus explicabo animi necessitatibus reiciendis eaque! Commodi deleniti exercitationem, in similique recusandae rerum doloremque odio,
+         earum a accusamus numquam beatae delectus est nemo enim? Dolore quos repudiandae eligendi error corrupti veniam in ex vel modi accusamus corporis, est id cumque mollitia iure itaque 
+         voluptatibus accusantium, inventore reprehenderit atque ab neque! Doloribus praesentium iure eligendi quibusdam laboriosam voluptate quidem, accusantium veniam molestias consequatur 
+         sapiente laborum reprehenderit laudantium nostrum neque delectus ea consectetur culpa nihil repellat! Nihil itaque ducimus, 
+        quia sed culpa quisquam laboriosam et ad quod sunt excepturi ratione atque?</p>
+    <hr>
+
+    <!--Párrafos HTML-->
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem enim vero tempore!</p>
+
+</body>
+</html>
